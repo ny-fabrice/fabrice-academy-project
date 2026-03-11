82d05f2c8b17c26d91e4239e847269d93c022467
Fabrice <fabrice.niyongabo@codeland.it>
2026-03-11 19:42:26 +0200

    log writer configurations

--- core/src/test/java/it/codeland/fabrice/core/schedulers/SimpleScheduledTaskTest.java
+++ core/src/test/java/it/codeland/fabrice/core/schedulers/SimpleScheduledTaskTest.java
@@ -52,7 +52,7 @@ class SimpleScheduledTaskTest {
         fixture.run();
 
         List<LoggingEvent> events = logger.getLoggingEvents();
-        assertEquals(1, events.size());
+        assertEquals(11, events.size());
         LoggingEvent event = events.get(0);
         assertEquals(Level.DEBUG, event.getLevel());
         assertEquals(1, event.getArguments().size());
